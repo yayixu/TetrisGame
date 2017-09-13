6a1f6ae07e8746b739fd715ef0cc6f39215f5ee8
Ian Wen <ywen@snapchat.com>
2017-09-13 12:21:12 -0700

    Use Jcomponent instead of JPanel

--- src/TetrisGame/GamePanel.java
+++ src/TetrisGame/GamePanel.java
@@ -14,7 +14,6 @@ public class GamePanel extends BoardedPanel{
     private List<Shape> shapes;
 
     public GamePanel() {
-        setLayout(null);
         setBackground(Color.white);
         // Added a pause button into Game Panel
         pauseButton = new JButton(PAUSE);
@@ -22,6 +21,15 @@ public class GamePanel extends BoardedPanel{
         add(pauseButton);
         pauseButton.setVisible(false);
         pauseButton.setBounds(70, 225, 100, 50);
+
+        Funk f = new Funk();
+        add(f);
+        f.setBounds(0, 0, 200, 300);
+
+        Funk f1 = new Funk();
+        add(f1);
+        f1.setBounds(100, 100, 200, 300);
+
         addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
                 pauseButton.setVisible(true);
@@ -38,12 +46,13 @@ public class GamePanel extends BoardedPanel{
         return (int)(x * UNIT);
     }
 
-    private static class Funk extends Component {
-
-        public void paint(Graphics g) {
-            super.paint(g);
-            g.drawRect(10, 10, 200, 200);
-            g.setColor(Color.RED);
+    private static class Funk extends JComponent {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D gg = (Graphics2D) g;
+            gg.setColor(Color.RED);
+            gg.fillRect(0, 0, 100, 100);
         }
     }
 }
