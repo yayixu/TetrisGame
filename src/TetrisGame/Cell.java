f0a3c9c57c33a8da6bd31267ac0ed2f10db9f3cb
Yayi Xu <yayixu@Yayis-MacBook-Pro.local>
2017-09-17 00:29:30 -0700

    Make advance of MoveDown Action and add MouseWheelListner for RoateAction.

--- src/TetrisGame/Cell.java
+++ src/TetrisGame/Cell.java
@@ -10,12 +10,12 @@ import static TetrisGame.GamePanel.convert;
  * Created by yayixu on 9/11/17.
  */
 public class Cell extends JComponent{
-    private int x, y, z;
 
-    public Cell(int x, int y, int z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    // cell's type
+    private int type;
+
+    public Cell(int type) {
+        this.type = type;
     }
 
     @Override
@@ -28,44 +28,44 @@ public class Cell extends JComponent{
         int[] xs = new int[3];
         int[] ys = new int[3];
         g.setColor(Color.black);
-        switch (z) {
+        switch (type) {
             case 0:
-                xs[0] = convert(x);
-                xs[1] = convert(x);
-                xs[2] = convert(x + 0.5);
-                ys[0] = convert(y);
-                ys[1] = convert(y + 1);
-                ys[2] = convert(y + 0.5);
+                xs[0] = 0;
+                xs[1] = 0;
+                xs[2] = convert(0.5);
+                ys[0] = 0;
+                ys[1] = convert(1);
+                ys[2] = convert(0.5);
                 g.fillPolygon(xs, ys, 3);
                 g.drawPolygon(xs, ys, 3);
                 break;
             case 1:
-                xs[0] = convert(x);
-                xs[1] = convert(x + 1);
-                xs[2] = convert(x + 0.5);
-                ys[0] = convert(y);
-                ys[1] = convert(y);
-                ys[2] = convert(y + 0.5);
+                xs[0] = 0;
+                xs[1] = convert(1);
+                xs[2] = convert(0.5);
+                ys[0] = 0;
+                ys[1] = 0;
+                ys[2] = convert( 0.5);
                 g.fillPolygon(xs, ys, 3);
                 g.drawPolygon(xs, ys, 3);
                 break;
             case 2:
-                xs[0] = convert(x + 1);
-                xs[1] = convert(x + 1);
-                xs[2] = convert(x + 0.5);
-                ys[0] = convert(y);
-                ys[1] = convert(y + 1);
-                ys[2] = convert(y + 0.5);
+                xs[0] = convert(1);
+                xs[1] = convert(1);
+                xs[2] = convert(0.5);
+                ys[0] = 0;
+                ys[1] = convert(1);
+                ys[2] = convert(0.5);
                 g.fillPolygon(xs, ys, 3);
                 g.drawPolygon(xs, ys, 3);
                 break;
             case 3:
-                xs[0] = convert(x);
-                xs[1] = convert(x + 1);
-                xs[2] = convert(x + 0.5);
-                ys[0] = convert(y + 1);
-                ys[1] = convert(y + 1) ;
-                ys[2] = convert(y + 0.5);
+                xs[0] = 0;
+                xs[1] = convert( 1);
+                xs[2] = convert(0.5);
+                ys[0] = convert(1);
+                ys[1] = convert(1) ;
+                ys[2] = convert(0.5);
                 g.fillPolygon(xs, ys, 3);
                 g.drawPolygon(xs, ys, 3);
                 break;
@@ -73,43 +73,37 @@ public class Cell extends JComponent{
         }
     }
 
-    public int getZ() {
-        return this.z;
+    public int getType() {
+        return this.type;
     }
 
-    public void setStartPoint(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
 
-    public void setZ(int z) {
-        this.z = z;
+    public void setType(int type) {
+        this.type = type;
     }
 
     @Override
     public boolean equals(Object obj) {
         if (!(obj instanceof Cell)) return false;
         Cell cell = (Cell) obj;
-        if (this.x == cell.x && this.y == cell.y && this.z == cell.z) return true;
+        if (this.type == cell.type) return true;
         return false;
     }
 
     @Override
     public int hashCode() {
         int hash = 3;
-        hash = 7 * hash + this.x;
-        hash = 7 * hash + this.y;
-        hash = 7 * hash + this.z;
+        hash = 7 * hash + this.type;
         return hash;
     }
 
     public void move(int direction) {
         Rectangle oldBounds = this.getBounds();
-        if (direction == 0) {
-            this.setBounds(oldBounds.x, oldBounds.y + UNIT, oldBounds.width, oldBounds.height);
-        } else if (direction == 1) {
+        if (direction == 0) {  // move down
+            this.setBounds(oldBounds.x, oldBounds.y + UNIT / 2, oldBounds.width, oldBounds.height);
+        } else if (direction == 1) { // move to left
             this.setBounds(oldBounds.x - UNIT, oldBounds.y, oldBounds.width, oldBounds.height);
-        }else if (direction == 2) {
+        }else if (direction == 2) { // move to right
             this.setBounds(oldBounds.x + UNIT, oldBounds.y, oldBounds.width, oldBounds.height);
         }
         repaint();
@@ -117,11 +111,10 @@ public class Cell extends JComponent{
 
     public void rotate(boolean isClockwise) {
         if (isClockwise) {
-            setZ((this.z + 1) % 4);
+            setType((this.type + 1) % 4);
         } else {
-            setZ((this.z + 3) % 4);
+            setType((this.type + 3) % 4);
         }
-        setBounds(x, y, UNIT, UNIT);
         repaint();
     }
 }
